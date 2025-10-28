55216b81debefd427de3d93d6102c835e5e796af
Dennis <dennis-farris@hotmail.com>
2025-10-28 11:18:52 -0400

    created model and schema for collections

---
+++ models/Collection.js
@@ -0,0 +1,19 @@
+import { model, Model, Schema } from "mongoose";
+
+const CollectionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 20
+    },
+    images: {
+        type: [String],
+        required: true, 
+        default: []
+    }
+})
+
+const Collection = model("Collection", CollectionSchema);
+
+export default Collection;
